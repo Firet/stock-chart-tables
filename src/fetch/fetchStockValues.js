248fc306f3e98c7dd6914610eaedfed1d48dbfb3
Firet <damianfederico@protonmail.com>
2024-05-10 16:18:14 -0300

    Remove useEffect, comments and unused code in fetchStockValues

--- src/fetch/fetchStockValues.js
+++ src/fetch/fetchStockValues.js
@@ -1,42 +1,25 @@
-import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { setData } from '../features/data/dataSlice';
 
 const FetchStockValues = () => {
 
+	const dispatch = useDispatch();
 
-// const [data, setData] = useState(null); // State to store fetched data
-const dispatch = useDispatch();
-const data = useSelector((state) => state.data); // Get data from Redux store
+		const fetchData = async () => {
+			try {
+				const response = await axios.get('https://api.twelvedata.com/time_series?symbol=TSLA&interval=5min&start_date=2021-04-16%2009:48:00&end_date=2021-04-16%2019:48:00&apikey=f5a3b0164cf24ed1b0e554793421d7c4');
+				const responseData = response.data;
+				setData(responseData);
+				dispatch(setData(responseData));
+			} catch (error) {
+				console.error(error);
+			}
+		};
 
-useEffect(() => {
-  const fetchData = async () => {
-	try {
-	  // const response = await axios.get('https://api.twelvedata.com/stocks?symbol=NFLX&source=docs');
-    const response = await axios.get('https://api.twelvedata.com/time_series?symbol=TSLA&interval=5min&start_date=2021-04-16%2009:48:00&end_date=2021-04-16%2019:48:00&apikey=f5a3b0164cf24ed1b0e554793421d7c4');
-	  const responseData = response.data;
-	  setData(responseData);
-    dispatch(setData(responseData)); // Dispatch action with fetched data
-	} catch (error) {
-	  console.error(error);
-	}
-  };
+		fetchData();
 
-  fetchData(); // Fetch data on component mount
-}, []); // Empty dependency array ensures data is fetched only once
-
-
-
-
-
-  console.log('data es ', data);
-
-  return (
-    <div>
-        <p>this is FetchStockValues</p>
-    </div>
-  );
+	return null;
 };
 
 export default FetchStockValues;
