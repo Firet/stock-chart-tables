cf409d99b1caeacb92ed8d5a6adb23d5ddd7d56a
Firet <damianfederico@protonmail.com>
2024-05-10 09:38:29 -0300

    Change api call, remove unused html and add axis titles to chart

--- src/chart.js
+++ src/chart.js
@@ -1,50 +1,63 @@
-import React, { useState } from 'react';
+import React, { Suspense, useState } from 'react';
 import HighchartsReact from 'highcharts-react-official';
 import Highcharts from "highcharts/highstock";
 import { useSelector } from 'react-redux';
 
 const Chart = () => {
-  const [hoverData, setHoverData] = useState(null);
-  const [chartOptions, setChartOptions] = useState({
-    xAxis: {
-      categories: ['A', 'B', 'C'],
-    },
-    series: [
-      { data: [1, 2, 3] }
-    ],
-    plotOptions: {
-      series: {
-        point: {
-          events: {
-            mouseOver(e){
-              setHoverData(e.target.category)
-            }
-          }
-        }
-      }
-    }
-  });
+	const [hoverData, setHoverData] = useState(null);
+	const [chartOptions, setChartOptions] = useState({
+		xAxis: {
+			title: {
+				text: 'Intervalo'
+			},
+			categories: ['A', 'B', 'C'],
+		},
+		yAxis: {
+			title: {
+				text: 'Cotización'
+			}
+		},
+		series: [
+			{ data: [1, 2, 3] }
+		],
+		plotOptions: {
+			series: {
+				point: {
+					events: {
+						mouseOver(e) {
+							setHoverData(e.target.category)
+						}
+					}
+				}
+			}
+		}
+	});
 
-  const updateSeries = () => {
-    setChartOptions({ 
-      series: [
-          { data: [Math.random() * 5, 2, 1]}
-        ]
-    });
-  }
-  const stockData = useSelector(state => state.data); // Obtiene los datos del estado
-  console.log('stockData es ', stockData);
-  return (
-    <div>
-        <HighchartsReact
-          highcharts={Highcharts}
-          options={chartOptions}
-          constructorType={"stockChart"}
-        />
-        <h3>Hovering over {hoverData}</h3>
-        <button onClick={updateSeries}>Update Series</button>
-      </div>
-    )
+	const updateSeries = () => {
+		setChartOptions({
+			series: [
+				{ data: [Math.random() * 5, 2, 1] }
+			]
+		});
+	}
+
+	const stockData = useSelector(state => state.data);
+	console.log('stockData es ', stockData);
+	if(!stockData) {
+		return <div>Loading...</div>;
+	}
+	return (
+		<div>
+			<h3>Nombre acción</h3>
+			<HighchartsReact
+				highcharts={Highcharts}
+				options={chartOptions}
+				constructorType={"stockChart"}
+			/>
+			<h3>Hovering over {hoverData}</h3>
+			<button onClick={updateSeries}>Update Series</button>
+		</div>
+	)
 }
 
 export default Chart;
--- src/fetch/fetchStockValues.js
+++ src/fetch/fetchStockValues.js
@@ -13,7 +13,8 @@ const data = useSelector((state) => state.data); // Get data from Redux store
 useEffect(() => {
   const fetchData = async () => {
 	try {
-	  const response = await axios.get('https://api.twelvedata.com/stocks?symbol=NFLX&source=docs');
+	  // const response = await axios.get('https://api.twelvedata.com/stocks?symbol=NFLX&source=docs');
+    const response = await axios.get('https://api.twelvedata.com/time_series?symbol=TSLA&interval=5min&start_date=2021-04-16%2009:48:00&end_date=2021-04-16%2019:48:00&apikey=f5a3b0164cf24ed1b0e554793421d7c4');
 	  const responseData = response.data;
 	  setData(responseData);
     dispatch(setData(responseData)); // Dispatch action with fetched data
