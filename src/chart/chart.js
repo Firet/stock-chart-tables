d54c22b4138259c76fb600adb050fa71be4933b9
Firet <damianfederico@protonmail.com>
2024-05-10 18:34:09 -0300

    Add several configurations to chart

--- src/chart/chart.js
+++ src/chart/chart.js
@@ -6,27 +6,105 @@ import { useSelector } from 'react-redux';
 const Chart = () => {
 	const stockData = useSelector((state) => state.data);
 	const dateTime = stockData?.data?.values.map(item => item.datetime)
+	const stockVolume = stockData?.data?.values.map(item => Number(item.volume));
 	const stockOpen = stockData?.data?.values.map(item => Number(item.open));
 
 	const chartOptions = {
+		title: {
+			text: `Activo ${stockData?.data?.meta?.symbol}`
+		},
+
+		subtitle: {
+			text: `Intervalo ${stockData?.data?.meta?.interval}`
+		},
+		rangeSelector: {
+			floating: true,
+			y: -65,
+			verticalAlign: 'bottom'
+		},
 		xAxis: {
 			title: {
 				text: "Intervalo",
 			},
 			categories: dateTime,
 		},
-		yAxis: {
+		yAxis: [{
 			title: {
 				text: "Cotización",
 			},
+			labels: {
+				align: 'left'
+			},
+			height: '80%',
+			resize: {
+				enabled: true
+			}
+		}, {
+			labels: {
+				align: 'left'
+			},
+			top: '80%',
+			height: '20%',
+			offset: 0
+		}],
+		tooltip: {
+			shape: 'square',
+			headerShape: 'callout',
+			borderWidth: 0,
+			shadow: false,
+			positioner: function (width, height, point) {
+				const chart = this.chart;
+				let position;
+
+				if (point.isHeader) {
+					position = {
+						x: Math.max(
+							// Left side limit
+							chart.plotLeft,
+							Math.min(
+								point.plotX + chart.plotLeft - width / 2,
+								// Right side limit
+								chart.chartWidth - width - chart.marginRight
+							)
+						),
+						y: point.plotY
+					};
+				} else {
+					position = {
+						x: point.series.chart.plotLeft,
+						y: point.series.yAxis.top - chart.plotTop
+					};
+				}
+
+				return position;
+			}
 		},
 		series: [
 			{
-				name: "Valor",
-				id: "1",
+				type: "column",
+				name: "Volumen",
+				id: "aapl-volume",
+				data: stockVolume,
+				yAxis: 1
+			},
+			{
+				name: "Open",
+				id: "open",
 				data: stockOpen,
 			},
 		],
+		responsive: {
+			rules: [{
+				condition: {
+					maxWidth: 800
+				},
+				chartOptions: {
+					rangeSelector: {
+						inputEnabled: false
+					}
+				}
+			}]
+		}
 	};
 
 	if (!stockData) {
@@ -34,8 +112,6 @@ const Chart = () => {
 	}
 	return (
 		<div>
-			<h3>Nombre activo {stockData?.data?.meta?.symbol}</h3>
-			<h3>Intervalo {stockData?.data?.meta?.interval}</h3>
 			<HighchartsReact
 				highcharts={Highcharts}
 				options={chartOptions}
