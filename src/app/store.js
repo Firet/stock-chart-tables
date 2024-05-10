0954e9888aee34e3cc5ddd1dd903ccc336bbbab0
Firet <damianfederico@protonmail.com>
2024-05-10 16:23:53 -0300

    Remove counter feature

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
 import dataReducer from '../features/data/dataSlice'; // Import your slice reducer
 
 export default configureStore({
   reducer: {
-    counter: counterReducer,
     data: dataReducer,
   },
 });
